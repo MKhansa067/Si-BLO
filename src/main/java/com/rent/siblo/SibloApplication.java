d4c65069b16d167683d4260ab1d26cda809c4914
MKhansa067 <105094399+MKhansa067@users.noreply.github.com>
2026-06-11 12:16:00 +0700

    initial commit

---
+++ src/main/java/com/rent/siblo/SibloApplication.java
@@ -0,0 +1,13 @@
+package com.rent.siblo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SibloApplication.class, args);
+    }
+
+}
